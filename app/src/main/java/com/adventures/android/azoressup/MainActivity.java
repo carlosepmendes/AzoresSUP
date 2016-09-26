3970dbc379115081c7e8457acefc8dcbdf07275c
carlosepmendes <carlosepmendes@gmail.com>
2016-09-26 16:58:13 +0000

    Added images and text

--- app/src/main/java/com/adventures/android/azoressup/MainActivity.java
+++ app/src/main/java/com/adventures/android/azoressup/MainActivity.java
@@ -1,13 +1,24 @@
 package com.adventures.android.azoressup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Using the src property on xml to load the images but they can be load like these
+    //ImageView profilePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //profilePhoto = (ImageView)findViewById(R.id.profilePhoto);
+
+        //profilePhoto.setImageResource(R.drawable.top);
+
     }
+
+
+
 }
